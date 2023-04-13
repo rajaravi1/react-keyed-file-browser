f4b5c476080dfb8ba577dfc21db17e4eec25082d
Raja Ravi <rravi@logitech.com>
2023-04-13 10:29:03 +0530

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,35 @@
-const path = require('path')
-const NodeExternals = require('webpack-node-externals')
+const path = require("path");
+const NodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'react-keyed-file-browser.js',
-    library: 'react-keyed-file-browser',
-    libraryTarget: 'umd',
+    path: path.join(__dirname, "/dist"),
+    filename: "react-keyed-file-browser.js",
+    library: "react-keyed-file-browser",
+    libraryTarget: "umd",
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        loader: 'esbuild-loader',
-        include: path.join(__dirname, './src/'),
+        loader: "esbuild-loader",
+        include: path.join(__dirname, "./src/"),
         options: {
-          loader: 'jsx',
-          target: 'es2015',
+          loader: "jsx",
+          target: "es2015",
         },
       },
       {
         test: /.*\.sass*/,
-        loader: ['style-loader', 'css-loader', 'sass-loader'],
-        include: path.join(__dirname, '/src'),
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+        include: path.join(__dirname, "/src"),
       },
     ],
   },
   externals: NodeExternals(),
-}
+};
